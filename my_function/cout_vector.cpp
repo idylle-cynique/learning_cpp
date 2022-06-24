2989377c08cd3d591fcc996c987b78b64b633ec9
anecdoche <65955152+idylle-cynique@users.noreply.github.com>
2022-06-24 23:25:02 +0900

    my_functionディレクトリを別途作成
    よく用いるコードは個別のコードファイルに逐一記述しなくてもいいようにしておく

---
+++ my_function/cout_vector.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+/* 
+    vectorを引数として受け取った時の処理手続きをstd::ostreamに追加して
+    pythonのprint()のようにcoutでvectorを一発出力できるようにする
+*/
+
+template<typename Type_>
+std::ostream& operator<<(ostream& os, vector<Type_>& coutvec){
+    cout << "{";
+    for(auto element: coutvec){
+        os << element << ", ";
+    }
+    cout << "}" << endl;
+    return os;
+}
+int main(void){
+    vector<int> vec = {1,2,3,5,7,12};
+    vector<double> d_vec = {3.14, 1.59};
+    cout << vec << endl;
+    cout << d_vec << endl;
+}
